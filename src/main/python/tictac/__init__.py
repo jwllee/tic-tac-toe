f72c93834e13db576c46ff3e15fb44ee8c59c0de
Wai Lam Jonathan Lee <jonathan.wailam.lee@gmail.com>
2019-06-06 10:44:14 +0200

    tictac package

---
+++ src/main/python/tictac/__init__.py
@@ -0,0 +1,13 @@
+from . import board
+from . import game
+from . import player
+from . import strategy
+from . import utils
+from . import view
+
+from .board import *
+from .game import *
+from .player import *
+from .strategy import *
+from .utils import *
+from .view import *
