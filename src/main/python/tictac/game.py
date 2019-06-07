c20d8722aa20d80134b1cbdf4470a59b40f46368
Wai Lam Jonathan Lee <jonathan.wailam.lee@gmail.com>
2019-06-07 12:16:13 +0200

    Implement basic game class

--- src/main/python/tictac/game.py
+++ src/main/python/tictac/game.py
@@ -127,12 +127,73 @@ class Game(ABC):
 class GameBasic(Game):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.score = {marker:0 for marker in Marker}
+        self.total_round = 0
 
     def setup(self):
         n_rows = self.game_configs[GameParameter.BOARD_DIM]
         n_cols = self.game_configs[GameParameter.BOARD_DIM]
-        self.board = board_mod.Board2d(n_rows, n_cols)
+        self.board = Board2d(n_rows, n_cols)
+        self.board.register_observer(self.view)
         super().setup()
+
+    def __reached_max_round(self):
+        # check if all the scores are the same
+        score_list = np.asarray(list(self.score.values()))
+        draw = (score_list == score_list[0]).all()
+        if not draw:
+            # compute the winner
+            max_score = -1
+            winner = None
+            for marker,player in self.marker2player.items():
+                score = self.score[marker]
+                self._winner = player if score > max_score else self._winner
+                max_score = score if score > max_score else max_score
+
+    def start_board(self):
+        round_msg = 'Round {}/{}'.format(self.total_round, 
+                                         self.game_configs[GameParameter.N_ROUNDS])
+        self.view.display_msg(round_msg)
+        super().start_board()
+
+    def end_board(self):
+        self.total_round += 1
+        # increment winner's score
+        marker_int = self.board.state - BoardState.CIRCLE_WIN
+        if marker_int >= 0:
+            marker = Marker(marker_int)
+            self.score[marker] += 1
+            if self.score[marker] >= self.game_configs[GameParameter.FIRST_TO]:
+                self._winner = self.marker2player[marker]
+                return
+
+        maxed_rounds = self.total_round >= self.game_configs[GameParameter.N_ROUNDS]
+        if maxed_rounds:
+            self.__reached_max_round()
+            return
+
+        # game not finished yet, restart board
+        self.board.restart()
+        self.start_board()
+
+    def start(self):
+        self.setup()
+        welcome_msg = 'Welcome to Tic Tac Toe'
+        self.view.display_msg(welcome_msg)
+        self.start_board()
+        self.end()
+
+    def end(self):
+        self.logger.info('Game end')
+        if self.winner is None:
+            msg = 'Draw game after {} games.'.format(self.total_round)
+        else:
+            msg = '{!s} ({!r}) has won {} rounds out of {} games.'
+            msg = msg.format(self.winner.name, 
+                             self.winner.marker,
+                             self.score[self.winner.marker],
+                             self.total_round)
+        self.view.display_msg(msg)
 
     def mark_cell(self, marker, loc):
         is_marked = self.board.mark_cell(marker, loc)
