6368e93ee04d4ba20ad5db5076ddca99a84f425f
Wai Lam Jonathan Lee <jonathan.wailam.lee@gmail.com>
2019-06-07 12:14:01 +0200

    Add CellLocation to generalize the idea of cell location in board

--- src/main/python/tictac/board.py
+++ src/main/python/tictac/board.py
@@ -1,6 +1,6 @@
 from enum import Enum, IntEnum
 from collections import namedtuple, Counter
-from abc import ABC, abstractmethod
+from abc import ABC, abstractmethod, abstractclassmethod
 import numpy as np
 from . import utils
 
@@ -14,7 +14,7 @@ __all__ = [
 
 
 SEP = ','
-Cell = namedtuple('Cell', ['row', 'col', 'val'])
+Cell = namedtuple('Cell', ['content', 'loc'])
 
 
 class Marker(IntEnum):
@@ -42,13 +42,39 @@ class BoardState(IntEnum):
     CROSS_WIN = 3
 
 
-class Board(ABC):
-    EMPTY = -1
+class BoardType(Enum):
+    def __str__(self):
+        return str(self.name).capitalize()
 
+    ORIGINAL = 0
+
+
+class CellLocation(ABC):
+    COORDINATE_FORMAT = ''
+
+    def __init__(self, board_type):
+        self.board_type = board_type
+        self.coordinate = dict()
+
+    #------------------------------------------------------------ 
+    # Default implementations 
+    #------------------------------------------------------------ 
+    def __str__(self):
+        return str(self.coordinate)
+    
+    #------------------------------------------------------------ 
+    # Abstract methods
+    #------------------------------------------------------------ 
+    @abstractclassmethod
+    def parse(self, s):
+        raise NotImplementedError('Please implement this method.')
+
+
+class Board(ABC):
     def __init__(self):
-        self.__state = BoardState.ONGOING
-        self.__coordinate_format = None
+        self._state = BoardState.ONGOING
         self.observers = list()
+        self.logger = utils.make_logger(Board.__name__)
 
     #------------------------------------------------------------ 
     # Observer pattern
@@ -68,19 +94,11 @@ class Board(ABC):
     #------------------------------------------------------------ 
     @property
     def state(self):
-        return self.__state
+        return self._state
 
     @state.setter
     def state(self, s):
         raise NotImplementedError('Cannot set board state.')
-
-    @property
-    def coordinate_format(self):
-        return self.__coordinate_format
-
-    @coordinate_format.setter
-    def coordinate_format(self, _format):
-        raise NotImplementedError('Cannot set board coordinate format!')
 
     #------------------------------------------------------------ 
     # Abstract methods
@@ -90,15 +108,19 @@ class Board(ABC):
         raise NotImplementedError('Please implement this method.')
 
     @abstractmethod
-    def is_cell_empty(self, row, col):
+    def is_cell_empty(self, loc):
         raise NotImplementedError('Please implement this method.')
 
     @abstractmethod
-    def get_cell(self, row, col):
+    def get_cell(self, loc):
         raise NotImplementedError('Please implement this method.')
 
     @abstractmethod
-    def is_move_valid(self, move):
+    def get_empty_cells(self):
+        raise NotImplementedError('Please implement this method.')
+
+    @abstractmethod
+    def get_filled_cells(self):
         raise NotImplementedError('Please implement this method.')
 
     @property
@@ -115,88 +137,122 @@ class Board(ABC):
 
 
 class Board2d(Board):
+    class CellLocation(CellLocation):
+        COORDINATE_FORMAT = 'r, c'
+        ROW = 'row'
+        COL = 'col'
+
+        def __init__(self, row, col):
+            super().__init__(BoardType.ORIGINAL)
+            self.coordinate[self.ROW] = row
+            self.coordinate[self.COL] = col
+
+        @property
+        def row(self):
+            return self.coordinate[self.ROW]
+
+        @property
+        def col(self):
+            return self.coordinate[self.COL]
+
+        @classmethod
+        def parse(self, s):
+            row, col = s.split(SEP)
+            return Board2d.CellLocation(int(row), int(col))
+
+        def __str__(self):
+            return '({}, {})'.format(self.row, self.col)
+
     def __init__(self, n_rows, n_cols):
         super().__init__()
         err_msg = 'Basic tic-tac-toe board has equal no. of rows and cols!'
         assert n_rows == n_cols, err_msg
-        self.__coordinate_format = 'r,c'
         self.n_rows = n_rows
         self.n_cols = n_cols
-        self.board = np.full((self.n_rows, self.n_cols), Board.EMPTY)
+        self.board = np.full((self.n_rows, self.n_cols), np.nan)
         self.row_count = np.full((len(Marker), self.n_rows), 0)
         self.col_count = np.full((len(Marker), self.n_cols), 0)
 
     def restart(self):
-        self.board = np.full((self.n_rows, self.n_cols), Board.EMPTY)
+        self.board = np.full((self.n_rows, self.n_cols), np.nan)
         self.row_count = np.full((len(Marker), self.n_rows), 0)
         self.col_count = np.full((len(Marker), self.n_cols), 0)
+        self._state = BoardState.ONGOING
 
     #------------------------------------------------------------ 
     # Implementation of abstract methods
     #------------------------------------------------------------ 
-    def is_cell_empty(self, row, col):
+    def is_cell_empty(self, loc):
+        utils.assert_isinstance('cell location', self.CellLocation, loc)
+        row, col = loc.row, loc.col
         if row < 0 or row > self.n_rows:
             raise ValueError('Row out of bounds [{}, {})'.format(0, self.n_rows))
         if col < 0 or col > self.n_cols:
             raise ValueError('Column out of bounds [{}, {})'.format(0, self.n_cols))
-        return self.board[row, col] == Board.EMPTY
+        return np.isnan(self.board[row, col])
 
-    def get_cell(self, row, col):
+    def get_cell(self, loc):
+        utils.assert_isinstance('cell location', self.CellLocation, loc)
+        row, col = loc.row, loc.col
         cell_v = self.board[row, col]
-        marker = None if cell_v == Board.EMPTY else cell_v
-        return Cell(row, col, marker)
+        marker = None if cell_v == np.nan else Marker(cell_v + Marker.CIRCLE)
+        return Cell(marker, loc)
 
-    def is_move_valid(self, move):
-        row, col = move.split(SEP)
-        return self.board[row, col] == Board.EMPTY
+    def get_empty_cells(self):
+        locs = np.argwhere(np.isnan(self.board))
+        cells = list()
+        self.logger.debug('Empty cell locs: {}'.format(locs))
+        for row, col in locs:
+            loc = self.CellLocation(row, col)
+            cell = Cell(np.nan, loc)
+            cells.append(cell)
+        return cells
+
+    def get_filled_cells(self):
+        locs = np.where(self.board != np.nan) 
+        locs = np.argwhere(~np.isnan(self.board))
+        cells = list()
+        for row, col in locs:
+            loc = self.CellLocation(row, col)
+            val = Marker.CIRCLE + self.board[row, col]
+            cell = Cell(val, loc)
+            cells.append(cell)
+        return cells
 
     @property
     def full(self):
-        return (self.board == Board.EMPTY).any()
+        return not np.isnan(self.board).any()
 
     def mark_cell(self, marker, loc):
+        self.logger.debug('Marking "{!r}" at {}'.format(marker, loc))
         utils.assert_isinstance('marker', Marker, marker)
-        err_msg = 'Game state is not ongoing: {}'.format(self.__current_state)
-        assert self.__current_state == BoardState.ONGOING, err_msg
+        err_msg = 'Game state is not ongoing: {}'.format(self.state)
+        assert self.state == BoardState.ONGOING, err_msg
+
+        utils.assert_isinstance('cell location', self.CellLocation, loc)
+        row, col = loc.row, loc.col
         
-        row, col = loc
         # check if it's already marked
-        if self.board[row, col] != Board.EMPTY:
+        if not np.isnan(self.board[row, col]):
             return False
         else:
             self.board[row, col] = marker
             # update row, column, and diagonal count
             self.row_count[marker, row] += 1
             self.col_count[marker, col] += 1
+            self.logger.debug('row count: \n{}'.format(self.row_count))
+            self.logger.debug('col count: \n{}'.format(self.col_count))
+            self.__update_state(row, col)
             # update observers
             self.notify_observers()
             return True
     #------------------------------------------------------------ 
 
-    def get_row_count(self, row):
-        if row < 0 or row > self.n_rows:
-            raise ValueError('Row out of bounds [{}, {})'.format(0, self.n_rows))
-        row_dict = dict()
-        for marker in Marker:
-            row_dict[marker] = self.row_count[marker, row]
-
-        return row_dict
-
-    def get_col_count(self, col):
-        if col < 0 or col > self.n_cols:
-            raise ValueError('Column out of bounds [{}, {})'.format(0, self.n_cols))
-
-        col_dict = dict()
-        for marker in Marker:
-            col_dict[marker] = self.col_count[marker, col]
-
-        return col_dict
-
     def get_diag_count(self, start_ind, left2right=True):
         if left2right:
-            diag = np.diag(mat, start_ind)
+            diag = np.diag(self.board, start_ind)
         else:
-            diag = np.diag(np.fliplr(mat), start_ind)
+            diag = np.diag(np.fliplr(self.board), start_ind)
 
         diag_dict = dict()
         for marker in Marker:
@@ -206,19 +262,27 @@ class Board2d(Board):
         return diag_dict
 
     def __update_state(self, row, col):
+        self.logger.debug('Updating board state')
         # 1. check row 
-        row_count = self.get_row_count(row)
-        for marker in Marker:
-            if row_count[marker] == self.n_rows:
-                self.__current_state = BoardState.CIRCLE_WIN + marker
-                return
+        winner = np.where(self.row_count[:, row] == self.n_rows)[0]
+        self.logger.debug('Winner at row {}: {}'.format(row, winner))
+        if winner.shape[0] > 0:
+            marker = Marker(winner[0])
+            self._state = BoardState(BoardState.CIRCLE_WIN.value + winner[0])
+            msg = '"{!r}" win at row {}'
+            msg = msg.format(marker, row)
+            self.logger.debug(msg)
+            return
 
         # 2. check col
-        col_count = self.get_col_count(col)
-        for marker in Marker:
-            if col_count[marker] == self.n_cols:
-                self.__current_state = BoardState.CIRCLE_WIN + marker
-                return
+        winner = np.where(self.col_count[:, col] == self.n_cols)[0]
+        if winner.shape[0] > 0:
+            marker = Marker(winner[0])
+            self._state = BoardState(BoardState.CIRCLE_WIN.value + winner[0])
+            msg = '"{!r}" win at col {}'
+            msg = msg.format(marker, col)
+            self.logger.debug(msg)
+            return 
 
         # 3. check diagonals
         if row == col: # left to right diagonal
@@ -226,17 +290,21 @@ class Board2d(Board):
             
             for marker in Marker:
                 if diag_count[marker] == self.n_rows:
-                    self.__current_state = BoardState.CIRCLE_WIN + marker
+                    self._state = BoardState(BoardState.CIRCLE_WIN + marker)
+                    msg = '"{!r}" win at diagonal'.format(marker)
+                    self.logger.debug(msg)
                     return
 
-        elif row + col == n_rows - 1: # right to left diagonal
-            diag_count = self.get_diag_count(n_rows - 1)
+        elif row + col == self.n_rows - 1: # right to left diagonal
+            diag_count = self.get_diag_count(self.n_rows - 1)
 
             for marker in Marker:
                 if diag_count[marker] == self.n_rows:
-                    self.__current_state = BoardState.CIRCLE_WIN + marker
+                    self._state = BoardState(BoardState.CIRCLE_WIN + marker)
+                    msg = '"{!r}" win at diagonal'.format(marker)
+                    self.logger.debug(msg)
                     return
 
         # 4. check if there is any more empty cells
-        if self.full():
-            self.__current_state = BoardState.DRAW
+        if self.full:
+            self._state = BoardState.DRAW
