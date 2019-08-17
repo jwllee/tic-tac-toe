8a6a3ea6ed8e93994bf87b26363813b82253ad13
Wai Lam Jonathan LEE <jonathan.wailam.lee@gmail.com>
2019-08-17 19:51:12 -0400

    More unit tests

--- xo/tests/test_win_state_utils.py
+++ xo/tests/test_win_state_utils.py
@@ -36,6 +36,25 @@ class WinStateUtilsTest(TestCase):
 
         assert wins == expected
 
+    def test_compute_win_state_str_row_3x4x3(self):
+        n_rows, n_cols, n_connects = 3, 4, 3
+        wins = utils.compute_win_state_str_row(n_rows, n_cols, n_connects)
+
+        expected = [
+            '000000000111',
+            '000000001110',
+            '000001110000',
+            '000011100000',
+            '011100000000',
+            '111000000000'
+        ]
+        
+        wins = sorted(wins)
+        expected = sorted(expected)
+
+        assert_msg = '{} != {}'.format(expected, wins)
+        assert expected == wins, assert_msg
+
     def test_compute_win_state_col_3x3x3(self):
         n_rows, n_cols, n_connects = 3, 3, 3
         wins = utils.compute_win_state_col(n_rows, n_cols, n_connects)
@@ -50,6 +69,25 @@ class WinStateUtilsTest(TestCase):
         expected = sorted(expected)
 
         assert wins == expected
+
+    def test_compute_win_state_str_col_4x3x3(self):
+        n_rows, n_cols, n_connects = 4, 3, 3
+        wins = utils.compute_win_state_str_col(n_rows, n_cols, n_connects)
+
+        expected = [
+            '100100100000',
+            '010010010000',
+            '001001001000',
+            '000100100100',
+            '000010010010',
+            '000001001001',
+        ]
+        
+        wins = sorted(wins)
+        expected = sorted(expected)
+
+        assert_msg = '{} != {}'.format(expected, wins)
+        assert expected == wins, assert_msg
 
     def test_compute_win_state_col_4x4x4(self):
         n_rows, n_cols, n_connects = 4, 4, 4
@@ -103,3 +141,98 @@ class WinStateUtilsTest(TestCase):
         expected = sorted(expected)
 
         assert wins == expected
+
+    def test_compute_win_state_str_diag_l2r_3x3x3(self):
+        n_rows, n_cols, n_connects = 3, 3, 3
+        wins = utils.compute_win_state_str_diag_l2r(n_rows, n_cols, n_connects)
+
+        expected = [
+            '100010001'
+        ]
+
+        assert_msg = '{} != {}'.format(expected, wins)
+        assert expected == wins, assert_msg
+
+    def test_compute_win_state_str_diag_l2r_4x4x4(self):
+        n_rows, n_cols, n_connects = 4, 4, 4
+        wins = utils.compute_win_state_str_diag_l2r(n_rows, n_cols, n_connects)
+
+        expected = [
+            '1000010000100001', # diagonals
+        ]
+
+        assert_msg = '{} != {}'.format(expected, wins)
+        assert expected == wins, assert_msg
+
+    def test_compute_win_state_str_diag_l2r_5x5x5(self):
+        n_rows, n_cols, n_connects = 5, 5, 5
+        wins = utils.compute_win_state_str_diag_l2r(n_rows, n_cols, n_connects)
+
+        expected = [
+            '1000001000001000001000001', # diagonals
+        ]
+
+        assert_msg = '{} != {}'.format(expected, wins)
+        assert expected == wins, assert_msg
+
+    def test_compute_win_state_str_diag_l2r_4x4x3(self):
+        n_rows, n_cols, n_connects = 4, 4, 3
+        wins = utils.compute_win_state_str_diag_l2r(n_rows, n_cols, n_connects)
+
+        expected = [
+            '0000010000100001', # diagonals
+            '1000010000100000', 
+            '0000100001000010',
+            '0100001000010000',
+        ]
+
+        wins = sorted(wins)
+        expected = sorted(expected)
+
+        assert_msg = '{} != {}'.format(expected, wins)
+        assert expected == wins, assert_msg
+
+    def test_compute_win_state_str_diag_r2l_3x3x3(self):
+        n_rows, n_cols, n_connects = 3, 3, 3
+        wins = utils.compute_win_state_str_diag_r2l(n_rows, n_cols, n_connects)
+
+        expected = [
+            '001010100'
+        ]
+
+        wins = sorted(wins)
+        expected = sorted(expected)
+
+        assert_msg = '{} != {}'.format(expected, wins)
+        assert expected == wins, assert_msg
+
+    def test_compute_win_state_str_diag_r2l_4x4x4(self):
+        n_rows, n_cols, n_connects = 4, 4, 4
+        wins = utils.compute_win_state_str_diag_r2l(n_rows, n_cols, n_connects)
+
+        expected = [
+            '0001001001001000',
+        ]
+
+        wins = sorted(wins)
+        expected = sorted(expected)
+
+        assert_msg = '{} != {}'.format(expected, wins)
+        assert expected == wins, assert_msg
+
+    def test_compute_win_state_str_diag_r2l_4x4x3(self):
+        n_rows, n_cols, n_connects = 4, 4, 3
+        wins = utils.compute_win_state_str_diag_r2l(n_rows, n_cols, n_connects)
+
+        expected = [
+            '0000000100100100',
+            '0000001001001000',
+            '0001001001000000',
+            '0010010010000000',
+        ]
+
+        wins = sorted(wins)
+        expected = sorted(expected)
+
+        assert_msg = '{} != {}'.format(expected, wins)
+        assert expected == wins, assert_msg
