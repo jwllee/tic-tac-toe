9b7fbfda91df7ba6bead5ba599088fcf48080fa8
Wai Lam Jonathan LEE <jonathan.wailam.lee@gmail.com>
2019-08-17 17:12:25 -0400

    Added logger

--- xo/utils.py
+++ xo/utils.py
@@ -1,4 +1,34 @@
 import time
+import logging, logging.config
+
+
+logging.config.dictConfig({
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        },
+    },
+    'handlers': {
+        'default': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard'
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['default'],
+            'level': 'INFO',
+            'propagate': True
+        },
+    }
+})
+
+
+def make_logger(name):
+    return logging.getLogger(name)
 
 
 def timeit(func):
